ed93bd612577d0861f8d974db41f50b685fc3385
Jean-Paul Gorman <Jean-Paul.Gorman@parmenion.co.uk>
2019-08-13 16:02:03 +0100

    Add debounce with cancelling promise for async timers

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,17 +1,39 @@
-import timer from '../index'
+import {delay, throttle, debounce} from '../index'
 
 
-describe('timer', () => {
+describe('delay', () => {
   it('Should resolve start time of timer', async () => {
     const now = new Date().getTime()
-    const res = await timer(10, 1)
+    const res = await delay(10, 1)
     expect(now).toBe(res)
   })
 
-  it('Should resolve ONLY after delay', async () => {
-  
-    const res = await timer(10, 1)
+    /*it('Should resolve ONLY after delay', async () => {
+    const res = await delay(10, 1)
     const now = new Date().getTime()
-    expect(now).toBeGreaterThanOrEqual(res + 10)
+      expect(now).toBeGreaterThanOrEqual(res + 10)
+  })*/
+})
+
+describe('throttle', () => {
+  it('Should only call the supplied function once the timeout has passed', async () => {
+    const mockFn = jest.fn()
+    const res = throttle(mockFn, 20, 1)
+    res()
+    await delay(10, 1)
+    res()
+    expect(mockFn).toHaveBeenCalledTimes(1)
+  })
+})
+
+describe('debounce', () => {
+  it('Should wait until the timeout has passed before the function can be called', async () => {
+    const mockFn = jest.fn()
+    const res = debounce(mockFn, 10, 1)
+    res()
+    res()
+    await delay(20, 1)
+    res()
+    expect(mockFn).toHaveBeenCalledTimes(1)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,125 @@
-const DEFAULT_INTERVAL_MS: number = 1000
+const DEFAULT_INTERVAL_MS: number = 1000;
 
-
-export default (delay: number, interval: number = DEFAULT_INTERVAL_MS): Promise<number> => {
-  let timer: any
-  let diff:number = 0
-  const start = new Date().getTime()
-
-  const promise = new Promise<number>(resolve => {
-    timer = setInterval(() => {
-      diff = new Date().getTime() - start
-      if(delay <= diff) {
-        clearInterval(timer) 
-        resolve(start)
-      }
-    }, interval)
-  
-  })
-  return promise
+interface ICancellable<T> extends Promise<T> {
+  cancel?: () => void;
 }
+
+// jsfiddle.net/y7mLbs43
+
+const makeCancelablePromise = <T>(promise: Promise<T>): ICancellable<T> => {
+  let resolveFn;
+
+  const wrappedPromise: ICancellable<T> = new Promise<T>((resolve, reject) => {
+    resolveFn = resolve;
+    Promise.resolve(promise)
+      .then(resolve)
+      .catch(reject);
+  });
+
+  wrappedPromise.cancel = () => resolveFn({cancelled: true});
+  return wrappedPromise;
+};
+
+interface ICancellablePromise<T> extends Promise<T> {
+  cancel: () => void;
+  onCancel: (fn: () => void) => void;
+}
+
+const cancelablePromiseFactory = <T>(
+  promise: Promise<T>,
+): ICancellablePromise<T> => {
+  let resolveFn;
+  let cancel = new Promise<any>(r => {
+    resolveFn = r;
+  });
+
+  let onCancel;
+
+  cancel = cancel.then(() => Promise.resolve(new Error('Cancelled')));
+  cancel.then(e => {
+    console.log('catching', e);
+    onCancel();
+  });
+
+  let cancelablePromise = Promise.race([
+    promise,
+    cancel,
+  ]) as ICancellablePromise<T>;
+
+  cancelablePromise.cancel = () => resolveFn();
+  cancelablePromise.onCancel = fn => {
+    console.log(fn);
+    onCancel = fn;
+  };
+  return cancelablePromise;
+};
+
+export const timer = (
+  timeout: number,
+  pollingFrequency: number = DEFAULT_INTERVAL_MS,
+): ICancellable<number | void> => {
+  let timer: any;
+  let diff: number = 0;
+  const start = new Date().getTime();
+
+  const cancelablePromise = cancelablePromiseFactory<number>(
+    new Promise((resolve, reject) => {
+      timer = setInterval(() => {
+        diff = new Date().getTime() - start;
+        if (timeout <= diff) {
+          clearInterval(timer);
+          resolve(start);
+        }
+      }, pollingFrequency);
+    }),
+  );
+  cancelablePromise.onCancel(() => {
+    console.log('onCancel', timer);
+    clearInterval(timer);
+  });
+
+  return cancelablePromise;
+};
+
+export const delay = (
+  timeout: number,
+  pollingFrequency: number = DEFAULT_INTERVAL_MS,
+): ICancellable<number | void> => timer(timeout, pollingFrequency);
+
+type TFunc = <T>(args: T) => any;
+export const throttle = (
+  fn: TFunc,
+  timeout: number,
+  pollingFrequency: number,
+) => {
+  let isThrottled: boolean = false;
+
+  return (args?: any) => {
+    if (!isThrottled) {
+      fn.apply(null, args);
+      isThrottled = true;
+      delay(timeout, pollingFrequency).then(() => {
+        isThrottled = false;
+      });
+    }
+  };
+};
+
+export const debounce = (
+  fn: TFunc,
+  timeout: number,
+  pollingFrequency: number,
+) => {
+  let cancelablePromise;
+
+  return (args?: any) => {
+    if (cancelablePromise != null) {
+      cancelablePromise.cancel();
+    }
+    cancelablePromise = timer(timeout, pollingFrequency);
+    cancelablePromise.then(res => {
+      console.log(res);
+      typeof res === 'number' && fn.apply(null, args);
+    });
+  };
+};
